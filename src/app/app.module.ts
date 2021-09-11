05d14560a86a882d2764d870a9b92b8127aa0208
Geraldo <geraldo.gja@gmail.com>
2021-09-11 11:41:35 -0300

    ajustes no component -read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './components/views/home/home.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 
 @NgModule({
@@ -25,7 +26,7 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     FooterComponent,
     NavComponent,
     HomeComponent,
-    CategoriaReadComponent
+    CategoriaReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,7 +38,8 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     MatIconModule,
     MatCardModule,
     MatTableModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
